8f6f8d4813b7b1b3919e5f3443175fb6cfd6fcb9
maxmytime <42465581+maxmytime@users.noreply.github.com>
2023-06-09 14:36:49 +0700

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -336,4 +336,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    const options = {
+        color: 'link',
+        isRange: true,
+        lang: 'ru',
+        dateFormat: 'MM.dd.yyyy',
+    };
+
+    // Initialize all input of date type.
+    const calendars = bulmaCalendar.attach('[name="order-date"]', options);
+
+    // Loop on each calendar initialized
+    calendars.forEach(calendar => {
+        // Add listener to select event
+        calendar.on('select', date => {
+            console.log(date);
+        });
+    });
+
+    // To access to bulmaCalendar instance of an element
+    // const element = document.querySelector('#my-element');
+    // if (element) {
+        // bulmaCalendar instance is available as element.bulmaCalendar
+    //     element.bulmaCalendar.on('select', datepicker => {
+    //         console.log(datepicker.data.value());
+    //     });
+    // }
+
 });
